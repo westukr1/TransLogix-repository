f684ab5ef99e3cb3200a06c6fd55f74b118794e7
ypyuryk <westukr1@gmail.com>
2024-08-15 21:41:13 +0300

    test commit

--- TransLogix_djangoProject/settings.py
+++ TransLogix_djangoProject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from TransLogix_djangoProject import apps
+from TransLogix_djangoProject.apps import TranslogixConfig
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'translogix.apps.TranslogixConfig',
+    'TransLogix_djangoProject.apps.TranslogixConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
